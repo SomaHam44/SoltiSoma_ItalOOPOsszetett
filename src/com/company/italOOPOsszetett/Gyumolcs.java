e5232b40d985763ee38a237104df15ad558e3ce0
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2021-10-31 00:09:01 +0200

    Gyümölcs osztály

---
+++ src/com/company/italOOPOsszetett/Gyumolcs.java
@@ -0,0 +1,24 @@
+package com.company.italOOPOsszetett;
+
+public class Gyumolcs {
+    private String fajta;
+    private String iz;
+
+    public Gyumolcs(String fajta, String iz) {
+        this.fajta = fajta;
+        this.iz = iz;
+    }
+
+    public String milyenFajtaju() {
+        return fajta;
+    }
+
+    public String milyenizu() {
+        return iz;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Gyümölcs: %s - íz: %s", this.fajta, this.iz);
+    }
+}
